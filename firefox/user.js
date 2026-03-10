77ff9972da4f8980d0e5e3dcecf182de5c2cb67d
[AJavier Shiordia <shiordia@amazon.com>
2026-03-10 09:31:40 -0600

    add macos defaults file

--- firefox/user.js
+++ firefox/user.js
@@ -1,3 +1,3 @@
 user_pref("browser.ctrlTab.sortByRecentlyUsed", true);
 user_pref("browser.tabs.insertAfterCurrent", true);
-user_pref("browser.urlbar.showSearchSuggestionsFirst", true);
+user_pref("browser.urlbar.showSearchSuggestionsFirst", false);
