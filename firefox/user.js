2db1b5560bbe814a22eff0b853d14103008d9e2f
[AJavier Shiordia <shiordia@amazon.com>
2026-03-10 09:34:34 -0600

    add search in background tab firefox

--- firefox/user.js
+++ firefox/user.js
@@ -1,3 +1,4 @@
 user_pref("browser.ctrlTab.sortByRecentlyUsed", true);
 user_pref("browser.tabs.insertAfterCurrent", true);
 user_pref("browser.urlbar.showSearchSuggestionsFirst", false);
+user_pref("browser.search.context.loadInBackground", true);
